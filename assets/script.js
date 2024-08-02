5edc64a5e760ac34ddcbdad593c5592b434fc545
edmarsevero <edmar.severo@gmail.com>
2024-08-02 08:17:13 -0300

    :tada: initial commit

---
+++ assets/script.js
@@ -0,0 +1,25 @@
+function toggleMode() {
+const html = document.documentElement
+html.classList.toggle('light')
+
+const img = document.querySelector("#profile img")
+
+if(html.classList.contains('light')){
+    img.setAttribute("src", "./assets/Avatar.png")
+    
+}
+else{
+    img.setAttribute("src", "./assets/avatar_so.png")
+}
+
+
+
+
+
+//if(html.classList.contains('light')){
+//    html.classList.remove('light')
+//} else{
+        //html.classList.add('light')
+    //}
+
+}   
